b7695812b0f2a1924947e8726c7b24a1070ee74b
Kenjitsu <ricorga_97@hotmail.com>
2022-07-21 01:04:37 -0500

    fixing problem with folder

---
+++ BlogCSharp/Data/Migrations/20220520034636_FisrtMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogCSharp.Data.Migrations
+{
+    public partial class FisrtMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
